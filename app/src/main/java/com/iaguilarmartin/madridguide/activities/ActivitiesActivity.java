da3a4ac8cf3efd72bb5245370ec81328d20ec103
Ivan Aguilar Martin <iaguilarmartin@outlook.com>
2017-01-04 18:39:03 +0100

    ActivitiesActivity and ActivityDetailActivity created

---
+++ app/src/main/java/com/iaguilarmartin/madridguide/activities/ActivitiesActivity.java
@@ -0,0 +1,15 @@
+package com.iaguilarmartin.madridguide.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.iaguilarmartin.madridguide.R;
+
+public class ActivitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities);
+    }
+}
